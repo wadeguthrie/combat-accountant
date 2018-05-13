1e9ecef58001f0fa10a7d1e6ad86d6eb9c04619f
Wade Guthrie <wade@adventure101.com>
2018-05-12 17:28:28 -0700

    Curses is working, getting single characters from the screen, and exiting on 'e'

--- ca.py
+++ ca.py
@@ -30,15 +30,30 @@ import curses
 #def do_stuff():  # Defines a function.
 
 class CaDisplay(object):
+
+    # NOTE: remember to call win.refresh()
+    # win.addstr(y, x, "String", attrib)
+    #   attrib can be curses.color_pair(1) or curses.A_REVERSE, ...
+    #   color_pair is setup using: curses.init_pair(1, # should be a symbol
+    #                                               curses.COLOR_RED, # fg
+    #                                               curses.COLOR_WHITE) # bg
+    #   only legal colors: 0:black, 1:red, 2:green, 3:yellow, 4:blue,
+    #                      5:magenta, 6:cyan, and 7:white
+    # 
+
     def __init__(self):
         self.__stdscr = None
 
     def __enter__(self):
         try:
             self.__stdscr = curses.initscr()
+            curses.noecho()
+            curses.cbreak() # respond instantly to keystrokes
+            self.__stdscr.keypad(1) # special characters converted by curses
+                                    # (e.g., curses.KEY_LEFT)
         except:
             curses.endwin()
-        return
+        return self
 
     def __exit__ (self, exception_type, exception_value, exception_traceback):
         if exception_type is IOError:
@@ -50,17 +65,31 @@ class CaDisplay(object):
             print 'EXCEPTION val: %s' % exception_value
             print 'Traceback: %r' % exception_traceback
 
+        self.__stdscr.keypad(0)
+        curses.nocbreak()
+        curses.echo()
         curses.endwin()
         self.__stdscr = None
         return True
+
+    def GetInput(self):
+        c = self.__stdscr.getch()
+        # 'c' will be something like ord('p') or curses.KEY_HOME
+        # TODO: convert 'c' to something ascii-like
+        return chr(c) # I _think_ converts it to ASCII
+
+    def Menu(self):
+        begin_x = 20; begin_y = 7
+        height = 5; width = 40
+        win = curses.newwin(height, width, begin_y, begin_x)
+
 
 
 # Main
 if __name__ == '__main__':
     with CaDisplay() as display:
-        pass
+        while display.GetInput() != 'e':
+            pass
 
-    #stdscr = curses.initscr()
-    #curses.endwin()
 
 
