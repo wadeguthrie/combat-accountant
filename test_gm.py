0b2c3d0eb7a58dea9f4bdae0ee966044f13ba177
Wade Guthrie <wade@adventure101.com>
2018-07-02 07:13:00 -0700

    Adds a second initiative test.  Passes.

--- test_gm.py
+++ test_gm.py
@@ -470,8 +470,6 @@ class EventTestCase(unittest.TestCase): # Derive from unittest.TestCase
         assert hand_to_hand_info['parry_skill'] == 10
 
     def test_initiative_order(self):
-        # random.randint(1, 6) should generate: 1 2 4 4 4 4 5 6 4 4
-        random.seed(9001) # 9001 is an arbitrary number
 
         random_debug_filename = 'foo'
 
@@ -517,22 +515,109 @@ class EventTestCase(unittest.TestCase): # Derive from unittest.TestCase
                     {'name': 'Pestilence', 'group': 'horsemen'}, # 5.5,  11, 4
                     {'name': 'Manny',      'group': 'PCs'}]      # 5.25, 10, 1
 
-        fight_handler = gm.FightHandler(self.__window_manager,
-                                        world,
-                                        'horsemen',
-                                        self.__ruleset,
-                                        random_debug_filename,
-                                        filename='*INTERNAL*',
-                                        maintain_json=False
-                                       )
-        fighters = fight_handler.get_fighters()
+        # Do this multiple times just to verify that the random stuff works
+        for i in range(10):
+            # random.randint(1, 6) should generate: 1 2 4 4 4 4 5 6 4 4
+            random.seed(9001) # 9001 is an arbitrary number
+            fight_handler = gm.FightHandler(self.__window_manager,
+                                            world,
+                                            'horsemen',
+                                            self.__ruleset,
+                                            random_debug_filename,
+                                            filename='*INTERNAL*',
+                                            maintain_json=False
+                                           )
+            fighters = fight_handler.get_fighters()
 
-        # Check the order against the one that I expect
+            # Check the order against the one that I expect
 
-        for index, ignore in enumerate(fighters):
-            assert fighters[index]['name'] == expected[index]['name']
-            assert fighters[index]['group'] == expected[index]['group']
+            for index, ignore in enumerate(fighters):
+                assert fighters[index]['name'] == expected[index]['name']
+                assert fighters[index]['group'] == expected[index]['group']
 
+    def test_initiative_order_again(self):
+        '''
+        This is just like test_initiative_order_again except the fighters are
+        reordered randomly and a different random seed is used.
+        '''
+
+        random_debug_filename = 'foo'
+
+        world = {
+            # Don't need templates, dead-monsters, equipment, names
+            'PCs': {
+                # 5.5, 11, rand=2
+                'Bob' : copy.deepcopy(self.__vodou_priest_fighter),
+
+                # 5.75, 12, rand=3
+                'Ted' : copy.deepcopy(self.__tank_fighter),
+            },
+            'monsters': {
+                'marx' : {
+                    # 5.5, 12, rand=4
+                    'Groucho' : copy.deepcopy(self.__one_more_guy),
+
+                    # 5.75, 12, rand=5
+                    'Harpo' : copy.deepcopy(self.__thief_fighter),
+
+                    # 5.25, 10, rand=3
+                    'Chico' : copy.deepcopy(self.__bokor_fighter),
+                }
+            },
+            'current-fight': {
+                # Needed
+                'saved': False,
+                'history': [], # Needed (maybe)
+
+                # Not needed if not saved
+                'index': 1,
+                'fighters': [],
+                'round': 2,
+                'monsters': 'marx',
+            },
+        }
+
+        # Famine and Jack have the same basic speed and dx -- it's up to rand
+        # Pestilence and Moe have same basic speed but different dx
+        expected = [
+                    {'name': 'Harpo',   'group': 'marx'}, # 5.75, 12, 5
+                    {'name': 'Ted',     'group': 'PCs'},  # 5.75, 12, 3
+                    {'name': 'Groucho', 'group': 'marx'}, # 5.5,  12, 4
+                    {'name': 'Bob',     'group': 'PCs'},  # 5.5,  11, 2
+                    {'name': 'Chico',   'group': 'marx'}, # 5.25, 10, 3
+                   ]
+
+        # Do this multiple times just to verify that the random stuff works
+        for i in range(10):
+            # random.randint(1, 6) should generate: 2 3 4 5 3
+            random.seed(8534) # 8534 is an arbitrary number
+            fight_handler = gm.FightHandler(self.__window_manager,
+                                            world,
+                                            'marx',
+                                            self.__ruleset,
+                                            random_debug_filename,
+                                            filename='*INTERNAL*',
+                                            maintain_json=False
+                                           )
+            fighters = fight_handler.get_fighters()
+
+            # Check the order against the one that I expect
+
+            for index, ignore in enumerate(fighters):
+                assert fighters[index]['name'] == expected[index]['name']
+                assert fighters[index]['group'] == expected[index]['group']
+
+    #def test_random_seed(self):
+    #    for i in range(10):
+    #        random.seed() # randomize
+    #        new_seed = random.randint(1, 10000)
+    #
+    #        print '\nseed: %d' % new_seed
+    #        for j in range(10):
+    #            random.seed(new_seed)
+    #            for k in range(5):
+    #                print random.randint(1, 6),
+    #            print ''
 
 if __name__ == '__main__':
     unittest.main() # runs all tests in this file
