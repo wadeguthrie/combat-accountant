01ab1f7a923e05ca58ff33e2499f8b00146f9a36
Wade Guthrie <wadeguthrie@users.noreply.github.com>
2021-09-08 15:43:18 -0700

    Bumps version number.  Passes tests.

--- ca.py
+++ ca.py
@@ -7062,7 +7062,7 @@ class Options(object):
 
 # Main
 if __name__ == '__main__':
-    VERSION = '00.04.00'    # major version, minor version, bug fixes
+    VERSION = '00.04.01 provisional'    # major version, minor version, bug fixes
 
     parser = MyArgumentParser()
     parser.add_argument(
